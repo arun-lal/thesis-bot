d9720130efd93516572c470fbe5c5ec424178b61
Arun Lal <arunlal2850@gmail.com>
2024-04-10 02:41:22 +0200

    add files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Thesis Bot',
+    version='0.0.0',
+    author='Arun Lal',
+    author_email='arunlal2850@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+
+)
